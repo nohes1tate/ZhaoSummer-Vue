46fdb0b43b1a71076bca0f57858d54105d497afa
nohesitate <20373441@buaa.edu.cn>
2022-08-01 10:56:46 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,8 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+import axios from 'axios'
+import qs from 'qs'
+Vue.prototype.$qs = qs;
 
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios;
+axios.defaults.baseURL = "http://43.138.86.76/api/";
+
+Vue.use(ElementUI)
+Vue.use(mavonEditor)
 
 new Vue({
   router,
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <tip-tap></tip-tap>
   </div>
 </template>
+
+<script>
+import TipTap from "@/components/TipTap";
+export default {
+  components: {TipTap},
+  data (){
+    return {
+      value: ''
+    }
+  }
+}
+</script>
