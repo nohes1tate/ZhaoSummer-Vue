0e345bed6199c58c0c8927d494c0c8285e50c783
nohesitate <20373441@buaa.edu.cn>
2022-08-03 17:40:51 +0800

    realTime

--- src/components/documentEdit/Realtime.js
+++ src/components/documentEdit/Realtime.js
@@ -1,13 +1,11 @@
 import { keymap } from 'prosemirror-keymap'
 import { Extension } from 'tiptap'
-import { redo, undo, ySyncPlugin, yUndoPlugin } from 'y-prosemirror'
-//import { WebsocketProvider } from 'y-websocket'
+import { redo, undo, yCursorPlugin, ySyncPlugin, yUndoPlugin } from 'y-prosemirror'
+import { WebsocketProvider } from 'y-websocket'
 import * as Y from 'yjs'
 
 const ydoc = new Y.Doc()
-const roomName = localStorage.getItem('roomName')
-console.log('websockRoom:',roomName)
-//const provider = new WebsocketProvider('wss://demos.yjs.dev', roomName, ydoc)
+const provider = new WebsocketProvider('wss://demos.yjs.dev', 'tiptap-demo', ydoc)
 const type = ydoc.getXmlFragment('prosemirror')
 
 export default class RealtimeExtension extends Extension {
@@ -15,10 +13,10 @@ export default class RealtimeExtension extends Extension {
     return 'realtime'
   }
 
-
   get plugins () {
     return [
       ySyncPlugin(type),
+      yCursorPlugin(provider.awareness),
       yUndoPlugin(),
       keymap({
         'Mod-z': undo,
