f656f3fd23a9e2ddcee5c427de5d1a2c16bcfa34
cYw <1147701986@qq.com>
2022-08-08 23:49:46 +0800

    welcome差不多完成，差个二维码

--- src/views/welcome/WelcomeView.vue
+++ src/views/welcome/WelcomeView.vue
@@ -176,7 +176,7 @@ export default {
       var timer = setInterval(function () {
         let osTop =
             document.documentElement.scrollTop || document.body.scrollTop;
-        let ispeed = Math.floor(-osTop / 5);
+        let ispeed = Math.floor(-osTop / 10);
         document.documentElement.scrollTop = document.body.scrollTop =
             osTop + ispeed;
         this.isTop = true;
